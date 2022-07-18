fbcddb5bbf9bf637652f32a51adeb26324a0e6da
Thimmapur <tvrajasekharreddy@gmail.com>
2022-07-18 19:34:31 +0530

    This is my first code push in git1

--- mongo1.py
+++ mongo1.py
@@ -4,10 +4,17 @@ db = client.test
 print(db)
 
 d = {"name": "Rajasekhar", "email":"raja@gmail.com","Surname": "Sekhar"}
+d = {"name": "Rajasekhar", "email":"raja@gmail.com","Surname": "Sekhar"}
+d = {"name": "Rajasekhar", "email":"raja@gmail.com","Surname": "Sekhar"}
+d = {"name": "Rajasekhar", "email":"raja@gmail.com","Surname": "Sekhar"}
+d = {"name": "Rajasekhar", "email":"raja@gmail.com","Surname": "Sekhar"}
+d = {"name": "Rajasekhar", "email":"raja@gmail.com","Surname": "Sekhar"}
+d = {"name": "Rajasekhar", "email":"raja@gmail.com","Surname": "Sekhar"}
+d = {"name": "Rajasekhar", "email":"raja@gmail.com","Surname": "Sekhar"}
 db1 = client["mongotest"]
 coll = db1['test']
 coll.insert_one(d)
-dlkbn
+
 
 
 
